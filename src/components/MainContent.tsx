b1c8b0e5b4844593f2957a697db0fa69d848d9f3
SebastianDiaz97 <sdiazr1997@gmail.com>
2025-05-01 16:46:58 -0400

    Card responsive

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -26,8 +26,9 @@ const MainContent = ({ location }: Props) => {
     return (
       <>
         <Heading textAlign={"center"}>{data?.resolvedAddress}</Heading>
-        <Grid templateColumns="repeat(5, 1fr)" gap="6" mt={10}>
-          {data?.days.slice(0, 10).map((day) => {
+        <Grid templateColumns={{ base: "1fr", sm: "repeat(2, 1fr)", md: "repeat(3, 1fr)" , lg:'repeat(4,1fr)'}} gap="6" mt={10}>
+          {data?.days.slice(0, 6
+          ).map((day) => {
             return (
               <CardWeather
                 key={day.datetime}
